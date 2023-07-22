3d08eccca7eaf08fc0edb8ce7702cea3ff370dcb
nikhilmirandaunbxd <nikhil.miranda@gmail.com>
2023-07-22 20:07:56 +0530

    feat: added the Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 
 const Header = () => {
+    /* Toggle Menu */
+    const [Toggle, showMenu] = useState(false);
+    
     return (
         <header className='header'>
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">Miranda</a>
 
-                <div className="nav__menu">
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#home" className="nav__link">
+                            <a href="#home" className="nav__link active-link">
                                 <i className="uil uil-estate nav__icon"></i> Home
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="" className="nav__link">
+                            <a href="#about" className="nav__link">
                                 <i className="uil uil-user nav__icon"></i> About
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="" className="nav__link">
+                            <a href="#skills" className="nav__link">
                                 <i className="uil uil-file-alt nav__icon"></i> Skills
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="" className="nav__link">
-                                <i className="uil uil-estate nav__icon"></i> Home
+                            <a href="#services" className="nav__link">
+                                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="uil uil-message nav__icon"></i> Contact
                             </a>
                         </li>
                     </ul>
 
-                    <i className="uil uil-times nav__close"></i>
-
-                    <div className="nav_-toggle">
-                        <i className="uil uil-apps"></i>
-                    </div>
+                    <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
                 </div>
 
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <i class="uil uil-apps"></i>
+                </div>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
