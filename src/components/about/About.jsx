f57003adcd2b40346456428c41f6aadd99080674
nikhilmirandaunbxd <nikhil.miranda@gmail.com>
2023-07-26 20:09:34 +0530

    feat: added skills and about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,8 +7,8 @@ import Info from './Info';
 const About = () => {
     return (
         <section className="about section" id="about">
-            <h2 className="section-title">About Me</h2>
-            <span className="section-subtitle">My_Introduction</span>
+            <h2 className="section__title">About Me</h2>
+            <span className="section__subtitle">My Introduction</span>
 
             <div className="about__container container grid">
                 <img src={aboutImg} alt="" className='about__img' />
